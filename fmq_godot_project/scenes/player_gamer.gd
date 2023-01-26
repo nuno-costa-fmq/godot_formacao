37d15998a2490eadcd34e9fceef19b28f2577def
Nuno Costa <nuno.costa@fabamaq.com>
2023-01-26 13:42:57 +0000

    Movimento OK

--- fmq_godot_project/scenes/player_gamer.gd
+++ fmq_godot_project/scenes/player_gamer.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 var move_speed : float = 50
 
+<<<<<<< Updated upstream
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("mouse_0_click"):
 	
@@ -11,6 +12,34 @@ func _process(delta: float) -> void:
 	rotation_degrees += 90
 	var dir = mouse_pos - position
 	move_and_slide(dir * move_speed)
+=======
+export (int) var speed = 200
+
+onready var target = position
+var velocity = Vector2()
+
+func _input(event):
+	if event.is_action_pressed("mouse_click_left"):
+		target = get_global_mouse_position()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
+func _physics_process(_delta):
+	velocity = position.direction_to(target) * speed
+	# look_at(target)
+	if position.distance_to(target) > 5:
+		velocity = move_and_slide(velocity)
+>>>>>>> Stashed changes
 
 
 #IGV Sessão 3 - Scripting 1 : 1:09:49
