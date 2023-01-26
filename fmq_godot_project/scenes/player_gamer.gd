4cf110ea8d12a16bf39c5f2cca4bf9aacc513c64
Nuno Costa <nuno.costa@fabamaq.com>
2023-01-26 10:25:15 +0000

    Player Mexe com point & click
    https://docs.godotengine.org/en/stable/tutorials/2d/2d_movement.html#click-and-move

--- fmq_godot_project/scenes/player_gamer.gd
+++ fmq_godot_project/scenes/player_gamer.gd
@@ -1,20 +1,27 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 var move_speed : float = 50
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("mouse_0_click"):
-	
-func _process(delta: float) -> void:
-	var mouse_pos = get_global_mouse_position()
-	look_at(mouse_pos)
-	rotation_degrees += 90
-	var dir = mouse_pos - position
-	move_and_slide(dir * move_speed)
+export (int) var speed = 200
+
+onready var target = position
+var velocity = Vector2()
+
+func _input(event):
+	if event.is_action_pressed("mouse_click_left"):
+		target = get_global_mouse_position()
+
+func _physics_process(delta):
+	velocity = position.direction_to(target) * speed
+	# look_at(target)
+	if position.distance_to(target) > 5:
+		velocity = move_and_slide(velocity)
 
 
 #IGV Sessão 3 - Scripting 1 : 1:09:49
 
 # https://www.youtube.com/watch?v=05OixHPbxNA
 # https://www.youtube.com/watch?v=SUZpVd18IMM&t=0s
+
+#https://docs.godotengine.org/en/stable/tutorials/2d/2d_movement.html#click-and-move
 
